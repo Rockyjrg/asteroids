35fd0ea708c3ac827a40b2dfd85ac219648879b7
Rockyjrg <jamie.g.ii@hotmail.com>
2025-07-06 21:03:37 -0500

    Added a split method in asteroids.py that split the large and medium asteroids on impact with the bullet. Finished with the asteroids game for now, might come back and add some things.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if asteroid.collision_check(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
